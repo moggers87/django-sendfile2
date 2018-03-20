69ff9343f93e79bff248f9f1759229a5683f86db
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-03-20 16:38:33 +0000

    Fix tests
    We need to mnaully specify sendfile in order for it to have its test
    run.

--- sendfile/backends/xsendfile.py
+++ sendfile/backends/xsendfile.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+import six
 
 
 def sendfile(request, filename, **kwargs):
+    filename = six.text_type(filename)
+    if six.PY2:
+        filename = filename.encode("utf-8")
+
     response = HttpResponse()
-    response['X-Sendfile'] = unicode(filename).encode('utf-8')
+    response['X-Sendfile'] = filename
 
     return response
