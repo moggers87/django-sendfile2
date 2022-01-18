340f9606bc6916cc118b39743b310dd6f4553b3c
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2022-01-18 20:38:04 +0000

    House keeping
    - Add Django 4.0 to test matrix
    - Add Python 3.10 to test matrix
    - Remove Pythons 3.5 and 3.6 from test matrix
    - Upgrade urls to re_path

--- examples/protected_downloads/download/urls.py
+++ examples/protected_downloads/download/urls.py
@@ -1,9 +1,9 @@
-from django.conf import urls
+from django import urls
 
 from .views import direct_download, download, download_list
 
 urlpatterns = [
-    urls.url(r'(?P<download_id>\d+)/$', download, name='download'),
-    urls.url(r'^$', download_list),
-    urls.url(r'direct/(?P<filename>.*)$', direct_download, name='direct_download'),
+    urls.re_path(r'(?P<download_id>\d+)/$', download, name='download'),
+    urls.re_path(r'^$', download_list),
+    urls.re_path(r'direct/(?P<filename>.*)$', direct_download, name='direct_download'),
 ]
